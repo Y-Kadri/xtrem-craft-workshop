8aa07f7ff03aaaaa5559c08a52105ef344f4f440
yKadri <yanis.kadri@etu.u-bordeaux.fr>
2023-09-12 12:05:04 +0200

    refactor

--- java/src/main/java/money_problem/domain/MoneyCalculator.java
+++ java/src/main/java/money_problem/domain/MoneyCalculator.java
@@ -1,15 +1,20 @@
 package money_problem.domain;
 
 public class MoneyCalculator {
-    public static double add(double amount, double amount2) {
+    public static double add(double amount, Currency currency, double amount2) {
+        // convert amount
         return amount + amount2;
     }
 
-    public static double multiplicate(double amount, int value) {
+    public static double multiply(double amount, Currency currency, int value) {
+        // convert amount
+        // todo not multiplying by negative number
         return amount * value;
     }
 
-    public static double divide(double amount, int value) {
+    public static double divide(double amount, Currency currency, int value) {
+        // convert amount (bank method)
+        // TODO Division by zero
         return amount / value;
     }
 }
--- java/src/test/java/money_problem/domain/MoneyCalculatorTest.java
+++ java/src/test/java/money_problem/domain/MoneyCalculatorTest.java
@@ -2,24 +2,25 @@ package money_problem.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static money_problem.domain.Currency.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MoneyCalculatorTest {
     @Test
     void shouldAddInUsd() {
-        assertThat(MoneyCalculator.add(5, 10))
+        assertThat(MoneyCalculator.add(5, USD, 10))
                 .isNotNull();
     }
 
     @Test
     void shouldMultiplyInEuros() {
-        assertThat(MoneyCalculator.multiplicate(10, 2))
+        assertThat(MoneyCalculator.multiply(10, EUR, 2))
                 .isEqualTo(20);
     }
 
     @Test
     void shouldDivideInKoreanWons() {
-        assertThat(MoneyCalculator.divide(4002, 4))
+        assertThat(MoneyCalculator.divide(4002, KRW, 4))
                 .isEqualTo(1000.5);
     }
 }
